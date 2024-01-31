06630033c3507c0f69d8eb92ff1219e7d1f18b51
Agung Firmansyah <agungfir98@gmail.com>
2024-01-31 11:32:57 +0700

    feat: adding ReactQueryWrapper and ProfileProvider

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,12 +1,26 @@
+"use server";
 import React from "react";
+import ProfileProvider from "~/components/ProfileProvider";
+import { ReactQueryProvider } from "~/components/ReactQueryProvider";
 import userSession from "~/lib/actions/guard";
 
 const MainLayout: React.FC<{
-  children: React.ReactNode;
-  landingpage: React.ReactNode;
+	children: React.ReactNode;
+	landingpage: React.ReactNode;
 }> = async ({ children, landingpage }) => {
-  const { data } = await userSession();
-  return <div>{data.session ? children : landingpage}</div>;
+	const { data } = await userSession();
+
+	return (
+		<>
+			{data.session ? (
+				<ReactQueryProvider>
+					<ProfileProvider>{children}</ProfileProvider>
+				</ReactQueryProvider>
+			) : (
+				landingpage
+			)}
+		</>
+	);
 };
 
 export default MainLayout;
