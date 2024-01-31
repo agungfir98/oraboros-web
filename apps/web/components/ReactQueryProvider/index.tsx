d7812f794ce42aa8097cff651a32cd5941ab42eb
Agung Firmansyah <agungfir98@gmail.com>
2024-01-31 11:44:01 +0700

    feat: react query provider

---
+++ apps/web/components/ReactQueryProvider/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+export const ReactQueryProvider: React.FC<{ children: React.ReactNode }> = ({
+	children,
+}) => {
+	return (
+		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+	);
+};
