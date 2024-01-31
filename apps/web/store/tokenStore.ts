33196d92cfb2b687ef25a15c6fc9f893da4cc7f0
Agung Firmansyah <agungfir98@gmail.com>
2024-01-31 11:46:02 +0700

    feat: token state store

---
+++ apps/web/store/tokenStore.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+type TokenStore = {
+	token: string;
+	// eslint-disable-next-line no-unused-vars
+	setToken: (arg: string) => void;
+};
+
+const useTokenStore = create<TokenStore>((set) => ({
+	token: "",
+	setToken(arg) {
+		set(() => ({ token: arg }));
+	},
+}));
+
+export default useTokenStore;
