e9c04219374024a9a18542fa943c901c2595ed9c
Agung Firmansyah <agungfir98@gmail.com>
2024-02-02 19:20:09 +0700

    fix: fix boarding route

--- apps/server/src/core/profile/profile.controller.ts
+++ apps/server/src/core/profile/profile.controller.ts
@@ -1,32 +1,96 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Put,
+  Query,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { ProfileService } from './profile.service';
-import { CreateProfileDTO } from '@ob/dto';
+import { CreateProfileDTO, EditProfileDTO } from '@ob/dto';
 import { SupabaseGuard } from '../auth/supabase/supabase.guard';
+import { Response } from 'express';
 
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @UseGuards(SupabaseGuard)
-  @Get(':id')
-  async getProfile(@Param() params: { id: string }) {
-    const user = await this.profileService.getUserById(params.id);
-    return user;
-  }
-
-  @Post('boarding')
-  async createNewUser(@Body() createProfileDTO: CreateProfileDTO) {
+  @Get('boarding')
+  async createNewUser(
+    @Query() createProfileDTO: CreateProfileDTO,
+    @Res() res: Response,
+  ) {
     const user = await this.profileService.getUserByEmail(
       createProfileDTO.email,
     );
 
-    if (!user) {
-      const newUser = await this.profileService.createUser({
-        displayName: createProfileDTO.displayName,
-        email: createProfileDTO.email,
+    try {
+      if (!user) {
+        const newUser = await this.profileService.createUser({
+          displayName: createProfileDTO.displayName,
+          email: createProfileDTO.email,
+        });
+
+        return res.status(201).json({
+          messsage: 'new user created',
+          newUser,
+        });
+      }
+
+      if (user._count.Budget === 0) {
+        return res.status(404).send();
+      }
+
+      return res.send();
+    } catch (error) {
+      return res.status(500).json({
+        message: 'failed to write data',
+        error,
       });
-      return newUser;
     }
-    return { ada: 'ud ada' };
+  }
+
+  @UseGuards(SupabaseGuard)
+  @Get(':id')
+  async getProfile(@Param() params: { id: string }) {
+    try {
+      const user = await this.profileService.getUserById(params.id);
+      return user;
+    } catch (error) {
+      console.log(error);
+      return {
+        msg: 'something wong with the server',
+        error,
+      };
+    }
+  }
+
+  @UseGuards(SupabaseGuard)
+  @Put(':id')
+  async editProfile(
+    @Param() params: { id: string },
+    @Body() editProfile: EditProfileDTO,
+  ) {
+    const editedUser = await this.profileService.editProfile(
+      params.id,
+      editProfile,
+    );
+
+    return editedUser;
+  }
+
+  @UseGuards(SupabaseGuard)
+  @Delete(':id')
+  async deleteProfile(@Param() params: { id: string }) {
+    const res = await this.profileService.deleteProfile(params.id);
+
+    return {
+      message: 'deleted',
+      res,
+    };
   }
 }
--- apps/server/src/core/profile/profile.service.ts
+++ apps/server/src/core/profile/profile.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/lib/prisma.service';
-import { CreateProfileDTO } from '@ob/dto';
+import { CreateProfileDTO, EditProfileDTO } from '@ob/dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ProfileService {
   constructor(private readonly prismaService: PrismaService) {}
 
   public async createUser(data: Omit<CreateProfileDTO, 'userId'>) {
-    return this.prismaService.profile.create({ data });
+    return this.prismaService.profile.create({
+      data,
+      include: { _count: true },
+    });
   }
 
   async getUserById(userId: string) {
@@ -22,5 +26,22 @@ export class ProfileService {
       where: { email },
       select: { _count: true, displayName: true, email: true },
     });
+  }
+
+  async editProfile(userId: string, editProfileDTO: EditProfileDTO) {
+    const data: Prisma.ProfileUpdateInput = {
+      ...editProfileDTO,
+    };
+
+    return await this.prismaService.profile.update({
+      where: {
+        userId,
+      },
+      data,
+    });
+  }
+
+  async deleteProfile(userId: string) {
+    return await this.prismaService.profile.delete({ where: { userId } });
   }
 }
