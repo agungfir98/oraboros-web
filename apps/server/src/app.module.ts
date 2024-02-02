b7fd542604071d8e2e4392433cb1016c9d562558
Agung Firmansyah <agungfir98@gmail.com>
2024-02-02 09:37:16 +0700

    chore: adding http execption filter to module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,10 +4,18 @@ import { AppService } from './app.service';
 import { ProfileModule } from './core/profile/profile.module';
 import { PassportModule } from '@nestjs/passport';
 import { SupabaseModule } from './core/auth/supabase/supabase.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExecptionFilter } from './http-execptionFilter';
 
 @Module({
   imports: [ProfileModule, PassportModule, SupabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExecptionFilter,
+    },
+  ],
 })
 export class AppModule {}
