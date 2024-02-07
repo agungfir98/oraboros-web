66fc158ec614e0e96171b071bca07697cd4541e9
Agung Firmansyah <agungfir98@gmail.com>
2024-02-07 13:06:44 +0700

    "chore: importing profile service and prisma service"

--- apps/server/src/core/auth/supabase/supabase.module.ts
+++ apps/server/src/core/auth/supabase/supabase.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SupabaseStrategy } from './supabase.strategy';
 import { SupabaseService } from 'src/lib/supabase.service';
+import { ProfileService } from 'src/core/profile/profile.service';
+import { PrismaService } from 'src/lib/prisma.service';
 
 @Module({
-  providers: [SupabaseStrategy, SupabaseService],
+  providers: [SupabaseStrategy, SupabaseService, ProfileService, PrismaService],
   exports: [SupabaseService],
 })
 export class SupabaseModule {}
