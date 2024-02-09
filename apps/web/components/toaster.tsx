227ecbd91c159d59262b69a254578b9f33469638
Agung Firmansyah <agungfir98@gmail.com>
2024-02-09 12:35:06 +0700

    feat: stylized toaster component

---
+++ apps/web/components/toaster.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Toaster } from "react-hot-toast";
+
+const CustomToaster = () => {
+	return (
+		<Toaster
+			toastOptions={{
+				className: "outline outline-1 outline-black",
+				style: {
+					borderRadius: 0,
+					boxShadow: "0.2rem 0.4rem black",
+				},
+				position: "bottom-right",
+			}}
+		/>
+	);
+};
+
+export default CustomToaster;
