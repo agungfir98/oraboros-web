6b725948e5e8c56174e03953468b83e8b90db498
Agung Firmansyah <agungfir98@gmail.com>
2024-02-09 14:28:01 +0700

    chore: adding session provider

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -3,24 +3,27 @@ import React from "react";
 import ProfileProvider from "~/components/ProfileProvider";
 import { ReactQueryProvider } from "~/components/ReactQueryProvider";
 import userSession from "~/lib/actions/guard";
+import { SessionProvider } from '~/components/sessionProvider'
 
 const MainLayout: React.FC<{
-	children: React.ReactNode;
-	landingpage: React.ReactNode;
+  children: React.ReactNode;
+  landingpage: React.ReactNode;
 }> = async ({ children, landingpage }) => {
-	const { data } = await userSession();
+  const { data } = await userSession();
 
-	return (
-		<>
-			{data.session ? (
-				<ReactQueryProvider>
-					<ProfileProvider>{children}</ProfileProvider>
-				</ReactQueryProvider>
-			) : (
-				landingpage
-			)}
-		</>
-	);
+  return (
+    <>
+      {data.session ? (
+        <ReactQueryProvider>
+          <SessionProvider>
+            <ProfileProvider>{children}</ProfileProvider>
+          </SessionProvider>
+        </ReactQueryProvider>
+      ) : (
+        landingpage
+      )}
+    </>
+  );
 };
 
 export default MainLayout;
