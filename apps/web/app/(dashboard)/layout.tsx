012c1dca8a8d355432659fbc883714879142fb3b
Agung Firmansyah <agungfir98@gmail.com>
2024-02-10 19:04:20 +0700

    "feat: adding custom toaster"

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import ProfileProvider from "~/components/ProfileProvider";
 import { ReactQueryProvider } from "~/components/ReactQueryProvider";
 import userSession from "~/lib/actions/guard";
-import { SessionProvider } from '~/components/sessionProvider'
+import { SessionProvider } from "~/components/sessionProvider";
+import CustomToaster from "~/components/toaster";
 
 const MainLayout: React.FC<{
   children: React.ReactNode;
@@ -15,6 +16,7 @@ const MainLayout: React.FC<{
     <>
       {data.session ? (
         <ReactQueryProvider>
+          <CustomToaster />
           <SessionProvider>
             <ProfileProvider>{children}</ProfileProvider>
           </SessionProvider>
