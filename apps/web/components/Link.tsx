d0e630cb9dee0fc19285b8c63875d1bf9f1ade91
Agung Firmansyah <agungfir98@gmail.com>
2024-02-13 21:07:57 +0700

    feat: Link custom component

---
+++ apps/web/components/Link.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import NextLink from "next/link";
+import type { LinkProps as DefaultLinkProps } from "next/link";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "~/utils/tailwind-merge";
+import { ClassValue } from "clsx";
+
+const LinkVariance = cva(
+	"flex items-center w-fit hover:underline hover:underline-offset-2",
+	{
+		variants: {
+			outline: {
+				none: "",
+				"1": "outline outline-black outline-1",
+				"2": "outline outline-black outline-2",
+				"3": "outline outline-black outline-3",
+			},
+		},
+		defaultVariants: {
+			outline: "none",
+		},
+	}
+);
+
+type LinkProps = {
+	children?: React.ReactNode;
+	className?: ClassValue;
+	target?: React.HTMLAttributeAnchorTarget;
+} & DefaultLinkProps &
+	VariantProps<typeof LinkVariance>;
+
+const Link: React.FC<LinkProps> = ({
+	children,
+	href,
+	className,
+	outline,
+	target,
+	...props
+}) => {
+	return (
+		<NextLink
+			href={href}
+			target={target ? target : "_blank"}
+			className={cn(LinkVariance({ className, outline }), className)}
+			{...props}
+		>
+			{children}
+		</NextLink>
+	);
+};
+
+export default Link;
