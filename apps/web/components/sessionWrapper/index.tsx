dcd1c08acb0e02a285cd2a4cd3932ffad80c9d14
Agung Firmansyah <agungfir98@gmail.com>
2024-03-02 16:03:04 +0700

    breaking: changing directory name from sessionProvider to sessionWrapper for better understanding

---
+++ apps/web/components/sessionWrapper/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { createSupabaseClientComponent } from "~/lib/supabase/client";
+import { useStore } from "~/store";
+
+export const SessionWrapper: React.FC<{ children: React.ReactNode }> = ({
+	children,
+}) => {
+	const router = useRouter();
+	const [loading, setLoading] = useState<boolean>(true);
+
+	const supabase = createSupabaseClientComponent();
+
+	const { onAuthSuccess, onLogout } = useStore();
+
+	useEffect(() => {
+		onLogout();
+		supabase.auth.getSession().then(({ data: { session } }) => {
+			if (session) {
+				setLoading(false);
+				onAuthSuccess({
+					accessToken: session.access_token,
+					user: session.user,
+				});
+			} else {
+				return router.replace("/");
+			}
+		});
+	}, []);
+
+	return !loading && children;
+};
