5749e0a4dd31ea29c5ef119c7707a5a54228d267
Agung Firmansyah <agungfir98@gmail.com>
2024-03-02 20:39:28 +0700

    feat: authStore

---
+++ apps/web/store/index.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { AuthSlice, createAuthSlice } from "./auth";
+
+export type GlobalStore = AuthSlice;
+
+const STORAGE_KEY = "oraboros_storage";
+
+export const useStore = create<
+	GlobalStore,
+	[["zustand/persist", Pick<GlobalStore, "accessToken">]]
+>(
+	persist(
+		(...a) => ({
+			...createAuthSlice(...a),
+		}),
+		{
+			name: STORAGE_KEY,
+			partialize: (state) => ({
+				accessToken: state.accessToken,
+				user: state.user,
+			}),
+		}
+	)
+);
