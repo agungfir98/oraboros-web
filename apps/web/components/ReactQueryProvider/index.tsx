5f9629dec4ad76189020c8cd32f8a254cb5f8e3d
Agung Firmansyah <agungfir98@gmail.com>
2024-03-02 19:48:38 +0700

    feat: adding ApiClientProvider for injecting axios in the packages directory

--- apps/web/components/ReactQueryProvider/index.tsx
+++ apps/web/components/ReactQueryProvider/index.tsx
@@ -1,13 +1,17 @@
 "use client";
-
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ApiClientProvider } from "@ob/api";
+import { AxiosManager } from "~/lib/axios";
 
+const axiosManager = new AxiosManager();
 const queryClient = new QueryClient();
 export const ReactQueryProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
+	children,
 }) => {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+	return (
+		<ApiClientProvider axiosInstace={axiosManager.axios}>
+			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		</ApiClientProvider>
+	);
 };
