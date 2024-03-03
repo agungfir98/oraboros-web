1d9aa0ad97a0e4f1caaf30a8d05b96172cfc2c36
Agung Firmansyah <agungfir98@gmail.com>
2024-03-03 12:24:42 +0700

    refactor: adjusting the session component sice it change name

--- apps/web/app/boarding-page/layout.tsx
+++ apps/web/app/boarding-page/layout.tsx
@@ -1,26 +1,29 @@
 import { Theme } from "@radix-ui/themes";
 import React from "react";
 import Navbar from "~/app/boarding-page/components/nav";
+import ProfileProvider from "~/components/ProfileProvider";
 import { ReactQueryProvider } from "~/components/ReactQueryProvider";
+import { SessionWrapper } from "~/components/sessionWrapper";
 import CustomToaster from "~/components/toaster";
-import { SessionProvider } from "~/components/sessionProvider";
 
 const BoardingLayoutPage: React.FC<{ children: React.ReactNode }> = async ({
-  children,
+	children,
 }) => {
-  return (
-    <Theme>
-      <CustomToaster />
-      <ReactQueryProvider>
-        <SessionProvider>
-          <div className="flex flex-col min-h-screen">
-            <Navbar />
-            {children}
-          </div>
-        </SessionProvider>
-      </ReactQueryProvider>
-    </Theme>
-  );
+	return (
+		<Theme>
+			<CustomToaster />
+			<ReactQueryProvider>
+				<SessionWrapper>
+					<ProfileProvider>
+						<div className="flex flex-col min-h-screen">
+							<Navbar />
+							{children}
+						</div>
+					</ProfileProvider>
+				</SessionWrapper>
+			</ReactQueryProvider>
+		</Theme>
+	);
 };
 
 export default BoardingLayoutPage;
