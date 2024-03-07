a1155aa994c8cc261417d0bfc0370494024a0710
Agung Firmansyah <agungfir98@gmail.com>
2024-03-07 12:56:18 +0700

    chore: remove repo packages

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,10 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-	ignorePatterns: ["./postcss.config.js", "./tailwind.config.js"],
-	root: true,
-	extends: ["@repo/eslint-config/next.js"],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		project: true,
-	},
+  ignorePatterns: ["./postcss.config.js", "./tailwind.config.ts"],
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
 };
