d86605ca41d2ee935cb473122ea726a7abe20f65
Agung Firmansyah <agungfir98@gmail.com>
2024-03-07 14:14:29 +0700

    "refactor: adjusting from cn function change of directory"

--- apps/web/components/Link.tsx
+++ apps/web/components/Link.tsx
@@ -2,51 +2,50 @@ import React from "react";
 import NextLink from "next/link";
 import type { LinkProps as DefaultLinkProps } from "next/link";
 import { VariantProps, cva } from "class-variance-authority";
-import { cn } from "~/utils/tailwind-merge";
 import { ClassValue } from "clsx";
+import { cn } from "~/lib/utils";
 
 const LinkVariance = cva(
-	"flex items-center w-fit hover:underline hover:underline-offset-2",
-	{
-		variants: {
-			outline: {
-				none: "",
-				"1": "outline outline-black outline-1",
-				"2": "outline outline-black outline-2",
-				"3": "outline outline-black outline-3",
-			},
-		},
-		defaultVariants: {
-			outline: "none",
-		},
-	}
+  "flex items-center w-fit hover:underline hover:underline-offset-2",
+  {
+    variants: {
+      outline: {
+        none: "",
+        "1": "outline outline-black outline-1",
+        "2": "outline outline-black outline-2",
+        "3": "outline outline-black outline-3",
+      },
+    },
+    defaultVariants: {
+      outline: "none",
+    },
+  },
 );
 
 type LinkProps = {
-	children?: React.ReactNode;
-	className?: ClassValue;
-	target?: React.HTMLAttributeAnchorTarget;
+  children?: React.ReactNode;
+  className?: ClassValue;
+  target?: React.HTMLAttributeAnchorTarget;
 } & DefaultLinkProps &
-	VariantProps<typeof LinkVariance>;
+  VariantProps<typeof LinkVariance>;
 
 const Link: React.FC<LinkProps> = ({
-	children,
-	href,
-	className,
-	outline,
-	target,
-	...props
+  children,
+  href,
+  className,
+  outline,
+  target,
+  ...props
 }) => {
-	return (
-		<NextLink
-			href={href}
-			target={target ? target : "_blank"}
-			className={cn(LinkVariance({ className, outline }), className)}
-			{...props}
-		>
-			{children}
-		</NextLink>
-	);
+  return (
+    <NextLink
+      href={href}
+      className={cn(LinkVariance({ className, outline }), className)}
+      {...props}
+    >
+      {children}
+    </NextLink>
+  );
 };
 
 export default Link;
