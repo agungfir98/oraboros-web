ea2eec4fcfb49697507edea15500dbb00259e612
Agung Firmansyah <agungfir98@gmail.com>
2024-03-07 12:55:26 +0700

    feat: shadcn ui component

--- apps/web/utils/tailwind-merge.ts
+++
@@ -1,7 +0,0 @@
-import { twMerge } from "tailwind-merge";
-
-import { clsx, ClassValue } from "clsx";
-
-export const cn = (...inputValue: ClassValue[]) => {
-  return twMerge(clsx(inputValue));
-};
