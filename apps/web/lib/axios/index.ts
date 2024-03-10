a34e932b4d4f4cedf19c058e3dab701fc723059b
Agung Firmansyah <agungfir98@gmail.com>
2024-03-10 17:12:26 +0700

    chore: change harcoded axios uri to env for prod

--- apps/web/lib/axios/index.ts
+++ apps/web/lib/axios/index.ts
@@ -1,32 +1,32 @@
 import Axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
-
+import { env } from "../env";
 import { useStore } from "~/store";
 
 export class AxiosManager {
-	public readonly axios: AxiosInstance;
+  public readonly axios: AxiosInstance;
 
-	constructor() {
-		this.axios = Axios.create({
-			baseURL: "http://localhost:8080",
-		});
+  constructor() {
+    this.axios = Axios.create({
+      baseURL: env.NEXT_PUBLIC_API_URI,
+    });
 
-		this.axios.interceptors.request.use(this.authRequestInterceptor);
-	}
+    this.axios.interceptors.request.use(this.authRequestInterceptor);
+  }
 
-	private async authRequestInterceptor(
-		axiosConfig: InternalAxiosRequestConfig
-	) {
-		const token = useStore.getState().accessToken;
+  private async authRequestInterceptor(
+    axiosConfig: InternalAxiosRequestConfig,
+  ) {
+    const token = useStore.getState().accessToken;
 
-		if (axiosConfig.headers) {
-			if (token) {
-				axiosConfig.headers.authorization = `Bearer ${token}`;
-			}
-			axiosConfig.headers.Accept = "application/json";
-		}
+    if (axiosConfig.headers) {
+      if (token) {
+        axiosConfig.headers.authorization = `Bearer ${token}`;
+      }
+      axiosConfig.headers.Accept = "application/json";
+    }
 
-		return axiosConfig;
-	}
+    return axiosConfig;
+  }
 }
 
 // will be used in SSR API Requests
--- apps/web/lib/env.ts
+++ apps/web/lib/env.ts
@@ -6,9 +6,11 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    NEXT_PUBLIC_API_URI: z.string(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_API_URI: process.env.NEXT_PUBLIC_API_URI,
   },
 });
